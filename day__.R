949b66293d627e2d49cbb6ad7d458aafc3a2684f
Evgeny Petrovsky <evgeny.petrovsky@ing.com>
2021-01-05 10:45:17 +0100

    update template

--- day__.R
+++ day__.R
@@ -20,11 +20,12 @@ dayXX_part1_solution <- function(input) {
   NULL
 }
 
-test_output_part1 <- -1
-test_result <- dayXX_part1_solution(test_input)
+exp_test_result <- -1
+act_test_result <- dayXX_part1_solution(test_input)
 print(paste(
-  "test result:", test_result,
-  "valid:", test_result == test_output_part1))
+  "actual test result:", act_test_result,
+  "expected result:", exp_test_result,
+  "valid:", act_test_result == exp_test_result))
 
 real_result_part1 <- dayXX_part1_solution(real_input)
 print(format(real_result_part1, scientific = FALSE))
@@ -35,11 +36,12 @@ dayXX_part2_solution <- function(input) {
   NULL
 }
 
-test_output_part2 <- -1
-test_result <- dayXX_part2_solution(test_input)
+exp_test_result <- -1
+act_test_result <- dayXX_part2_solution(test_input)
 print(paste(
-  "test result:", test_result,
-  "valid:", test_result == test_output_part2))
+  "actual test result:", act_test_result,
+  "expected result:", exp_test_result,
+  "valid:", act_test_result == exp_test_result))
 
 real_result_part2 <- dayXX_part2_solution(real_input)
 print(format(real_result_part2, scientific = FALSE))
