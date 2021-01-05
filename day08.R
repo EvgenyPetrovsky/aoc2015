5a2ad52d796bf729661b4c43230bf51edf9fc595
Evgeny Petrovsky <evgeny.petrovsky@ing.com>
2021-01-05 10:40:49 +0100

    day 8 part 2 solution

--- day08.R
+++ day08.R
@@ -23,6 +23,8 @@ total_length <- function(x) {
 
 eval_text <- function(text) eval(parse(text = text))
 
+serialize_text <- function(text) capture.output({text}) %>% substring(5)
+
 #- SOLUTION PART 1 ------------------------------------------------------------#
 
 day08_part1_solution <- function(input) {
@@ -48,13 +50,21 @@ print(format(real_result_part1, scientific = FALSE))
 #- SOLUTION PART 2 ------------------------------------------------------------#
 
 day08_part2_solution <- function(input) {
-  NULL
+  txt_lines <- 
+    input %>%
+    parse_input()
+  
+  text_len <- txt_lines %>% total_length()
+  srlz_len <- txt_lines %>% Map(f = serialize_text) %>% total_length()
+  
+  srlz_len - text_len
 }
 
-test_output_part2 <- -1
+test_output_part2 <-  (6 + 9 + 16 + 11) - (2 + 5 + 10 + 6)
 test_result <- day08_part2_solution(test_input)
 print(paste(
-  "test result:", test_result,
+  "actual test result:", test_result,
+  "expected test result:", test_output_part2,
   "valid:", test_result == test_output_part2))
 
 real_result_part2 <- day08_part2_solution(real_input)
